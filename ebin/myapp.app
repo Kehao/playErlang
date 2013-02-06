654751ee51a37c3077dd641f49d90e6b7c867822
麦飞 <kehao.qiu@gmail.com>
2013-02-06 15:24:48 +0800

    add my_bank test

--- ebin/myapp.app
+++ ebin/myapp.app
@@ -5,4 +5,4 @@
               {applications,[kernel,stdlib]},
               {mod,{myapp_app,[]}},
               {env,[]},
-              {modules,[myapp_app,myapp_sup]}]}.
+              {modules,[my_bank,myapp_app,myapp_sup,test]}]}.
--- src/myapp_app.erl
+++ src/myapp_app.erl
@@ -12,8 +12,8 @@
 %% ===================================================================
 %% Application callbacks
 
-start(_StartType, _StartArgs) ->
 %% ===================================================================
+start(_StartType, _StartArgs) ->
     myapp_sup:start_link().
 
 stop(_State) ->
